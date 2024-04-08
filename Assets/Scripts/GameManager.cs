7898c582d2e5c165aad8a162a1d140586e5aa653
brendanwhite1101052 <bw1101052@student.francistuttle.edu>
2024-04-08 13:37:45 -0500

    Main Menu (testing)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    public static void LoadScene(string newSceneName)
+    {
+        SceneManager.LoadScene(newSceneName);
+    }
+
+    public static void Quit()
+    {
+        Application.Quit();
+    }
+}
+
