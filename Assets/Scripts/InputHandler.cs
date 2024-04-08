9221d669c2a72788091cda4ac61d1b8665a748d6
brendanwhite1101052 <bw1101052@student.francistuttle.edu>
2024-04-08 12:50:21 -0500

    Initial commit

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+
+    PlayerMove playerMove;
+    FPCam fpCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMove = GetComponent<PlayerMove>();
+        fpCam = GetComponent<FPCam>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMoveInput();
+        HandleCameraInput();
+    }
+
+    void HandleMoveInput()
+    {
+        float forwardInput = Input.GetAxisRaw("Vertical");
+        float rightInput = Input.GetAxisRaw("Horizontal");
+
+        playerMove.AddMoveInput(forwardInput,rightInput);
+    }
+
+    void HandleCameraInput()
+    {
+        fpCam.AddXAxisInput(Input.GetAxis("Mouse Y") * Time.deltaTime);
+        fpCam.AddYAxisInput(Input.GetAxis("Mouse X") * Time.deltaTime);
+    }
+}
